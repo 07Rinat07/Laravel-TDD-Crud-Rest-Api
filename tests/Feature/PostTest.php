1784dcaec534f4efd8684f63ec2ba17b90e19b5d
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-04 21:30:10 +0500

    Post Title Validity Test__45-51m"

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Feature;
 
- use App\Models\Post;
- use Illuminate\Foundation\Testing\DatabaseTransactions;
- use Illuminate\Foundation\Testing\RefreshDatabase;
- use Illuminate\Http\Testing\File;
- use Illuminate\Support\Facades\Storage;
- use Tests\TestCase;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Testing\File;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
 
- class PostTest extends TestCase
+class PostTest extends TestCase
 {
     use DatabaseTransactions;
 
@@ -36,12 +36,24 @@ namespace Tests\Feature;
 
         $post = Post::first();
 
-        $this->assertEquals($data[ 'title'], $post->title);
-        $this->assertEquals($data[ 'description'], $post->description);
+        $this->assertEquals($data['title'], $post->title);
+        $this->assertEquals($data['description'], $post->description);
 
-        $this->assertEquals( 'images/' . $file->hashName(), $post->image_url);
+        $this->assertEquals('images/' . $file->hashName(), $post->image_url);
         Storage::disk('local')->assertExists($post->image_url);
 
     }
 
+    /** @test */
+
+    public function attribute_title_is_required_for_storing_post()
+    {
+//        $this->withoutExceptionHandling(); //при валидации и нужно увидеть все ошибки это убирать
+        $data = [
+            'title' => '',
+            'description' => 'Description'
+        ];
+        $res = $this->post('/posts', $data);
+        $res->assertRedirect();
+    }
 }
