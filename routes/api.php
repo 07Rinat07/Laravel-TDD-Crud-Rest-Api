3c09af951ebefa3df98be94e14a1b4c814bfbed9
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-05 23:37:31 +0500

    API post update test__1:47-1:53

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/posts', [PostController::class, 'store']);
+Route::patch('/posts/{post}', [PostController::class, 'update']);
